d255cf9ef1447dec5c16fbc66522ce5b6daf353d
Antoni Gorski <agorski@c1r9s3.42warsaw.pl>
2024-03-19 15:51:46 +0100

    strdup added

--- libft/ft_strdup.c
+++ libft/ft_strdup.c
@@ -1,27 +1,26 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_calloc.c                                        :+:      :+:    :+:   */
+/*   ft_strdup.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: agorski <agorski@student.42warsaw.pl>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/03/19 12:44:19 by agorski           #+#    #+#             */
-/*   Updated: 2024/03/19 14:16:27 by agorski          ###   ########.fr       */
+/*   Created: 2024/03/19 14:22:14 by agorski           #+#    #+#             */
+/*   Updated: 2024/03/19 15:47:54 by agorski          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-void	*ft_calloc(size_t nmemb, size_t size)
+char	*ft_strdup(const char *s)
 {
-	void *ptr;
-	if (nmemb && SIZE_MAX / nmemb < size)
-	{
+	size_t	len;
+	char	*new;
+
+	len = ft_strlen(s);
+	new = (char *)malloc(len + 1);
+	if (new == NULL)
 		return (NULL);
-	}
-	ptr = malloc(nmemb * size);
-	if (ptr == NULL)
-		return (NULL);
-	ft_bzero(ptr, (nmemb * size));
-	return (ptr);
+	ft_strlcpy(new, (char *)s, len + 1);
+	return (new);
 }
--- libft/libft.h
+++ libft/libft.h
@@ -6,12 +6,13 @@
 /*   By: agorski <agorski@student.42warsaw.pl>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/06 18:24:01 by agorski           #+#    #+#             */
-/*   Updated: 2024/03/17 22:19:19 by agorski          ###   ########.fr       */
+/*   Updated: 2024/03/19 14:23:47 by agorski          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef LIBFT_H
 # define LIBFT_H
+# include <stdint.h>
 # include <stdlib.h>
 
 int		ft_isalpha(int c);
@@ -35,5 +36,7 @@ void	*ft_memchr(const void *s, int c, size_t n);
 int		ft_memcmp(const void *s1, const void *s2, size_t n);
 char	*ft_strnstr(const char *big, const char *little, size_t len);
 int		ft_atoi(const char *nptr);
+void	*ft_calloc(size_t nmemb, size_t size);
+char	*ft_strdup(const char *s);
 
 #endif
