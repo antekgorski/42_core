f5d537a5147acdf16f5fdf23c8befa343553ecb0
Antoni Gorski <agorski@c2r1s3.42warsaw.pl>
2024-03-26 21:39:33 +0100

    split start

--- Libft_test/test_ft_strtrim.c
+++ Libft_test/test_ft_strtrim.c
@@ -15,7 +15,7 @@ int main()
 
     // Test case 2: Trim leading and trailing characters from set
     char str2[] = "###Hello, World!###";
-    char set2[] = "#";
+    char set2[] = "#i#";
     char *trimmed2 = ft_strtrim(str2, set2);
     printf("Trimmed string: '%s'\n", trimmed2); // Expected output: "Hello, World"
 
@@ -24,6 +24,12 @@ int main()
     char set3[] = " ";
     char *trimmed3 = ft_strtrim(str3, set3);
     printf("Trimmed string: '%s'\n", trimmed3); // Expected output: "Hello, World!"
+
+    // Test case 4: trimming all
+    char str4[] = "Hello, World!";
+    char set4[] = "Hello, World!";
+    char *trimmed4 = ft_strtrim(str4, set4);
+    printf("Trimmed string: '%s'\n", trimmed4); // Expected output: "Hello, World!"
 
     return 0;
 }
--- libft/ft_split.c
+++ libft/ft_split.c
@@ -6,11 +6,44 @@
 /*   By: agorski <agorski@student.42warsaw.pl>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/21 15:07:58 by agorski           #+#    #+#             */
-/*   Updated: 2024/03/21 15:09:00 by agorski          ###   ########.fr       */
+/*   Updated: 2024/03/26 21:37:40 by agorski          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-char **ft_split(char const *s, char c)
+#include "libft.h"
+#include <stdio.h>
+
+static int	ft_number_of_words(char const *s, char c)
 {
-    
+	size_t	i;
+	int		num;
+
+	num = 0;
+	i = 0;
+	if (*s != c && *s != '\0')
+		num++;
+	while (*(s + i) != '\0')
+	{
+		if (*(s + i) == c && *(s + i + 1) != c && *(s + i + 1) != '\0')
+			num++;
+		i++;
+	}
+	return (num);
+}
+
+char	**ft_split(char const *s, char c)
+{
+	int	word_number;
+
+	word_number = ft_number_of_words(s, c);
+	return (NULL);
+}
+
+int	main(void)
+{
+	printf("%d\n", ft_number_of_words("a  a", ' '));
+	printf("%d\n", ft_number_of_words("  jeden   dwa trzy cztery    piec    ",
+			' '));
+
+	return (0);
 }
